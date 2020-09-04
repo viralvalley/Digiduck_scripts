a0259a3a1c2c5d91be4c5b57198bbb4e8543b728
viralvalley <viralvalley2@gmail.com>
2020-09-04 13:16:28 -0500

    Update comp_info.ps1

--- comp_info.ps1
+++ comp_info.ps1
@@ -1,5 +1,4 @@
-New-Item test.txt
-Add-Content test.txt "Current User"
+Get-WmiObject -Class Win32_ComputerSystem > test.txt
+Add-Content test.txt "Current User: "
 $env:USERNAME >> test.txt
-Get-WmiObject -Class Win32_ComputerSystem >> test.txt
-ipconfig >> test.txt
+ipconfig >> test.txt 
